57f812cc5a90c3a48ac830c1a7af8facb376c3aa
vadim <1234@yrtdsf.ru>
2023-10-01 13:13:01 +0300

    Work3

---
+++ Work3/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Напишите программу, которая принимает на вход длину массива и в последующих строках элементы массива.
+5
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+3
+6, 1, 33 -> [6, 1, 33]
+*/
+
+Console.WriteLine("Введите размер массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = new int[a];
+
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine("Введите число массива: ");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+
+Console.Write("Итог: [");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(" " + Method(i) + " ");
+}
+Console.Write("]");
+
+int Method(int a)
+{
+    return numbers[a];
+}
